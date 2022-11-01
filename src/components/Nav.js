4d885b91e73ea4a7525d2b073c363ab9e1209326
Ian Hart <ianhart13@gmail.com>
2022-11-01 15:30:52 -0400

    built out and styled navbar and import material-ui

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Badge from "@material-ui/core/Badge";
 
 function Nav() {
-  return <div>Nav</div>;
+  return (
+		<nav className='sticky'>
+			<div className='logo-header'>HORLOGES</div>
+			<ul className='nav-links'>
+				<li>Home</li>
+				<li>Shop</li>
+				<li>
+					<Badge color="primary" badgeContent='0'>
+						<ShoppingCartIcon />{" "}
+					</Badge></li>
+			</ul>
+		</nav>
+	);
 }
 
 export default Nav;
